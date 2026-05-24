87aaf6ac6f1357f680863ce1eb364d110e14c855
hatotank <hatotank@gmail.com>
2026-05-24 13:53:54 +0900

    update 土曜日のイベント作成ロジックの修正

--- tool/MakeSchedule_JapaneseSaturdayBlue.ps1
+++ tool/MakeSchedule_JapaneseSaturdayBlue.ps1
@@ -4,9 +4,6 @@ $OUTPUT_FILE = $PWD.Path + "\BACalendar\ics\" + $OUTPUT_FILENAME
 $CRLF = "`r`n"
 $PRODID = "hatotank/BACalendar v1.0"
 $START_DAY = (Get-Date "2023/01/01")
-$END_DAY   = (Get-Date "2030/12/31")
-
-$day = $START_DAY
 
 Write-Host "start make" $OUTPUT_FILENAME
 $body  = "BEGIN:VCALENDAR" + $CRLF
@@ -14,23 +11,22 @@ $body += "PRODID:-//" + $PRODID + "//EN" + $CRLF
 $body += "VERSION:2.0" + $CRLF
 $body += "METHOD:PUBLISH" + $CRLF
 
-while($day.CompareTo($END_DAY) -le 0)
-{
-    if($day.DayOfWeek -eq "Saturday")
-    {
-        $body += "BEGIN:VEVENT" + $CRLF
-        $body += "DTSTAMP:" + (Get-Date).ToString("yyyyMMddTHHmmssZ") + $CRLF
-        $body += "TRANSP:TRANSPARENT" + $CRLF
-        $body += "UID:" + (New-Guid).ToString() + $CRLF
-        $body += "SUMMARY:*Japanese_Saturday_Blue" + $CRLF
-        $body += "CATEGORIES:Japanese_Saturday_Blue" + $CRLF
-        $body += "LAST-MODIFIED:" + (Get-Date).ToString("yyyyMMddHHmmssZ") + $CRLF
-        $body += "DTSTART;VALUE=DATE:" + ($day).ToString("yyyyMMdd") + $CRLF
-        $body += "DTEND;VALUE=DATE:" + ($day).AddDays(1).ToString("yyyyMMdd") + $CRLF
-        $body += "END:VEVENT" + $CRLF
-    }
-    $day = $day.AddDays(1)
-}
+# START_DAY以降の最初の土曜日を求める
+$day = $START_DAY
+while($day.DayOfWeek -ne "Saturday") { $day = $day.AddDays(1) }
+
+$body += "BEGIN:VEVENT" + $CRLF
+$body += "DTSTAMP:" + (Get-Date).ToString("yyyyMMddTHHmmssZ") + $CRLF
+$body += "TRANSP:TRANSPARENT" + $CRLF
+$body += "UID:" + (New-Guid).ToString() + $CRLF
+$body += "SUMMARY:*Japanese_Saturday_Blue" + $CRLF
+$body += "CATEGORIES:Japanese_Saturday_Blue" + $CRLF
+$body += "LAST-MODIFIED:" + (Get-Date).ToString("yyyyMMddTHHmmssZ") + $CRLF
+$body += "RRULE:FREQ=WEEKLY;BYDAY=SA" + $CRLF
+$body += "DTSTART;VALUE=DATE:" + ($day).ToString("yyyyMMdd") + $CRLF
+$body += "DTEND;VALUE=DATE:" + ($day).AddDays(1).ToString("yyyyMMdd") + $CRLF
+$body += "END:VEVENT" + $CRLF
+
 $body += "END:VCALENDAR" + $CRLF
 
 $body | ForEach-Object{ $_+"`n" } | ForEach-Object{ [Text.Encoding]::UTF8.GetBytes($_) } | Set-Content -Encoding Byte -Path $OUTPUT_FILE
